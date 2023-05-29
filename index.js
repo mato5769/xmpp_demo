75c6a3bab180ba9e718af85f99a126b16e7b4ec7
Matic Pesjak <matic.pesjak@student.um.si>
2023-05-29 19:58:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const xmpp = require('simple-xmpp')
+
+xmpp.on('online', data => {
+    console.log('You are online!')
+    console.log(`Connected as ${data.jid.user}`)
+    send()
+})
+xmpp.on("error", error => {
+    console.log(`Something went wrong!\n${error}`)
+})
+xmpp.on("chat", (from, msg) => {
+    console.log(`Got massage : ${msg} from ${from}`)
+})
+
+function send() {
+    setTimeout(send, 5000);
+    xmpp.send('matic@localhost', `Hi! ${Date.now()}`)
+}
+
+xmpp.connect({
+    "jid": "admin@localhost",
+    "password": "password",
+    "host": "localhost",
+    "port": 5222
+})
